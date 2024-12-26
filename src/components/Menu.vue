3831f311a24aec48006b24ffe5a0e5ce12e54625
a.dolgov <dolgov_am@mail.ru>
2024-12-26 17:07:48 +0300

    create

---
+++ src/components/Menu.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+name: 'Menu',
+  data(){
+  return {
+  }
+}}
+</script>
+
+<template>
+  <div class="menu">
+    hello
+  </div>
+</template>
+
+<style scoped>
+
+</style>
