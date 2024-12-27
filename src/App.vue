25a839a864d2cd3963318045754306ca485f9616
a.dolgov <dolgov_am@mail.ru>
2024-12-27 15:11:40 +0300

    create new views

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,13 @@
-<script>
-
-export default {
-  data() {
-    return {
-      title: 'Счетчик: ',
-      count: 0,
-      startTime: 0,
-      currentTime: 0,
-    }
-  },
-  methods: {}
-}
+<script setup>
+import Menu from "@/components/Menu.vue";
+import Counter from './done_projects_vue/Counter.vue'
 </script>
 
 <template>
-  <div class="counter_clicker ">
-    <div class="counter_clicker_texts">
-      <h3> {{ this.title }} </h3>
-      <h3> {{ this.count }} </h3>
-    </div>
-    <div class="counter_clicker_buttons">
-      <button class="btn btn-success" type="button" @click="this.count++" id="plus">Добавить</button>
-      <button class="btn btn-danger" type="button" @click="this.count--" id="minus">Вычесть</button>
-    </div>
-  </div>
+тут основное окно действий
+  <Counter/>
 </template>
 
 <style>
-h3, button.btn-danger, button.btn-success {
-  font-family: "Press Start 2P", serif;
-  font-style: normal;
-  color: white;
-}
-
-.counter_clicker {
-  display: block;
-  background-color: darkgrey;
-  border-radius: 10px;
-
-}
-.counter_clicker_texts, .counter_clicker_buttons {
-  display: flex;
-  justify-content: space-between;
-  margin: 10px 50px;
-  padding-inline-start: 10px;
-}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Menu from "@/components/Menu.vue";
+import Footer from "@/components/Footer.vue";
+import Header from "@/components/Header.vue";
 
 createApp(App).mount('#app')
+createApp(Menu).mount('#menu')
+createApp(Footer).mount('#footer')
+createApp(Header).mount('#header')
